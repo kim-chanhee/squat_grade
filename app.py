4a53726989ef22b9fa491f565f03d21cbd91c798
kim-chanhee <116836230+kim-chanhee@users.noreply.github.com>
2024-03-05 10:15:58 +0900

    add_flask2

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ import requests
 import base64
 import pandas as pd
 import csv, pymysql, logging
-
+import numpy as np
+import cv2
+from flask import Flask, request, jsonify
+import tensorflow as tf
+from collections import Counter
 from flask import Flask, render_template, request, redirect, url_for, session
 app = Flask(__name__)
 app.secret_key = "your_secret_key"
@@ -84,7 +88,68 @@ def home():
     U_id = session['login_user']
     return render_template('home.html', error=error, U_id=U_id)
 
+# ===============================
+# 이미지 디렉토리 경로
+img_dir = 'C:/CH_Project/자세교정/Model'
+model_path = os.path.join(img_dir, 'CNN_model.h5')
+model = tf.keras.models.load_model(model_path)
+
+# 이미지 전처리 함수
+def preprocess_image(img):
+    img = tf.image.resize(img, (32, 32), method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+    return img
+
+# 이미지 분류 함수
+def classify_image(img):
+    # 이미지 전처리
+    processed_image = preprocess_image(img)
+    # 이미지를 모델에 입력하여 예측 수행
+    prediction = model.predict(tf.expand_dims(processed_image, axis=0))
+    predicted_class = np.argmax(prediction, axis=1) + 1
+    return predicted_class
+
+# --------------------------------
+@app.route('/upload', methods=['POST'])
+def upload():
+    if 'image' not in request.files:
+        return 'No file part'
+    
+    # 클라이언트로부터 이미지를 받음
+    image_file = request.files['image']
+    
+    # 이미지 파일을 numpy 배열로 변환
+    nparr = np.frombuffer(image_file.read(), np.uint8)
+    
+    # numpy 배열을 OpenCV 이미지로 변환
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    
+    # 이미지를 분류하여 클래스 예측
+    predicted_classes = classify_image(img)
+    
+    # 클래스 수 세기
+    class_counts = Counter(predicted_classes)
+    count_4 = class_counts.get(4, 0)
+    
+    # 등급 지정
+    if count_4 >= 30:
+        grade = 'F'
+    elif count_4 >= 25:
+        grade = 'E'
+    elif count_4 >= 20:
+        grade = 'D'
+    elif count_4 >= 15:
+        grade = 'C'
+    elif count_4 >= 10:
+        grade = 'B'
+    else:
+        grade = 'A'
+
+    return render_template('home.html', grade=grade)
+
+    # 예측된 클래스와 등급을 JSON 형식으로 반환
+    # return jsonify({'grade': grade})  # 예측된 클래스 : 'predicted_classes': predicted_classes.tolist(),
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5052)
